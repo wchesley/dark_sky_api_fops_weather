2e20f7f122455e150d687941d5bb5e0432a4685a
Walker <Walker@DESKTOP-B3MJB36>
2017-06-22 12:58:48 -0500

    init commit

---
+++ dark_sky_weather/WeatherToDatabase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Newtonsoft.Json; 
+
+namespace dark_sky_weather
+{
+    public class WeatherToDatabase
+    {
+        static void SaveWeather(string raw_json_response, string city)
+        {
+            //class will take raw response as string from dark sky
+            //deserialize to WeatherJSON class 
+            //saves all variables to Database. 
+            var json_response = JsonConvert.DeserializeObject<WeatherJSON.RootObject>(raw_json_response);
+            string sql_connection = "server location";
+            string table_name = city;
+            string sql_query;
+            using (SqlConnection conn = new SqlConnection(sql_connection))
+            {
+                // there are several lists in the response from Dark Sky, will need for loops
+                //to iterate over all of em to insert data. 
+                //will grab current weather, minutly, hourly, daily and any alerts. 
+                sql_query = $"INSERT INTO {table_name} ()";
+                foreach(var hour in json_response.hourly.data)
+                {
+
+                }
+            }  
+        }
+    }
+}
