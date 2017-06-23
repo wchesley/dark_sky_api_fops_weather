0f6f9f30025ac467dbcc5caa0f5038e0b64f35ea
Walker <walker@Walkers-MacBook-Pro.local>
2017-06-22 23:10:52 -0500

    adding Insert Parameters.

--- dark_sky_weather/WeatherToDatabase.cs
+++ dark_sky_weather/WeatherToDatabase.cs
@@ -64,6 +64,8 @@ namespace dark_sky_weather
                 sql_query += "@current_apparentTemperature, @current_dewPoint, @current_humidity, @current_windSpeed, ";
                 sql_query += "@current_windGuest, @current_windBearing, @current_visibility, @current_cloudCover, ";
                 sql_query += "@current_pressure, @current_ozone, @current_uvIndex, ";
+                //end of Current Values from dark sky response 
+
                 sql_query += "@minutely_time, @minutely_precipIntensity, @minutely_precipProbability, @minutely_precipType, @minutely_summary, ";
                 //End of minutly values from Dark Sky response
                 
@@ -71,7 +73,7 @@ namespace dark_sky_weather
                 sql_query += "@hourly_temperature, @hourly_apparentTemperature, @hourly_dewPoint, @hourly_humidity, ";
                 sql_query += "@hourly_windSpeed, @hourly_windGust, @hourly_windBearing, @hourly_visibility, ";
                 sql_query += "@hourly_cloudCover, @hourly_pressure, @hourly_ozone, @hourly_uvIndex, @hourly_precipType, ";
-                sql_query += "@hourly_summary, ";
+                sql_query += "@hourly_summary_no_list, ";
                 //End hourly values from Dark Sky response
 
                 sql_query += "@daily_time, @daily_summary, @daily_sunriseTime, @daily_sunsetTime, ";
@@ -89,7 +91,108 @@ namespace dark_sky_weather
                 using(SqlCommand Insert = new SqlCommand(sql_query, conn))
                 {
                     //params outside of lists
-                    Insert.Parameters.AddWithValue();
+                    
+                    //current values: 
+                    Insert.Parameters.AddWithValue("@current_time", json_response.currently.time);
+                    Insert.Parameters.AddWithValue("@current_summary", json_response.currently.summary);
+                    Insert.Parameters.AddWithValue("@current_nearestStormDistance", json_response.currently.nearestStormDistance);
+                    Insert.Parameters.AddWithValue("@current_nearestStormBearing", json_response.currently.nearestStormBearing);
+                    Insert.Parameters.AddWithValue("@current_precipIntensity", json_response.currently.precipIntensity);
+                    Insert.Parameters.AddWithValue("@current_precipProbability", json_response.currently.precipProbability);
+                    Insert.Parameters.AddWithValue("@current_temperature", json_response.currently.temperature);
+                    Insert.Parameters.AddWithValue("@current_apparentTemperature", json_response.currently.apparentTemperature);
+                    Insert.Parameters.AddWithValue("@current_dewPoint", json_response.currently.dewPoint);
+                    Insert.Parameters.AddWithValue("@current_humidity", json_response.currently.humidity);
+                    Insert.Parameters.AddWithValue("@current_windSpeed", json_response.currently.windSpeed);
+                    Insert.Parameters.AddWithValue("@current_windGuest", json_response.currently.windGust);
+                    Insert.Parameters.AddWithValue("@current_windBearing", json_response.currently.windBearing);
+                    Insert.Parameters.AddWithValue("@current_visibility", json_response.currently.visibility);
+                    Insert.Parameters.AddWithValue("@current_pressure", json_response.currently.pressure);
+                    Insert.Parameters.AddWithValue("@current_cloudCover", json_response.currently.cloudCover);
+                    Insert.Parameters.AddWithValue("@current_ozone", json_response.currently.ozone);
+                    Insert.Parameters.AddWithValue("@current_uvIndex", json_response.currently.uvIndex);
+
+
+                    //cannot store list in SQL, will concantinate strings
+                    //together to store all values.
+                    string minute_time = string.Empty; 
+                    string minute_precipIntensity = string.Empty;
+                    string minute_precipProbability = string.Empty;
+                    string minute_precipType = string.Empty; 
+                    //minutely values: 
+                    
+                    foreach(var minute in json_response.Minutely.data)
+                    {
+                        minute_time += json_response.Minutely.data.time.ToString() + ", ";
+                        minute_precipIntensity += json_response.Minutely.data.precipIntensity.ToString() + ", ";
+                        minute_precipProbability += json_response.Minutely.data.precipProbability.ToString() + ", ";
+                        minute_precipType += json_response.Minutely.data.precipType + ", ";
+                    }
+                    Insert.Parameters.AddWithValue("@minutely_time", minute_time);
+                    Insert.Parameters.AddWithValue("@minutely_precipIntensity", minute_precipIntensity);
+                    Insert.Parameters.AddWithValue("@minutely_precipProbability", minute_precipProbability);
+                    Insert.Parameters.AddWithValue("@minutely_precipType", minute_precipType);
+                    Insert.Parameters.AddWithValue("@minutely_summary", json_response.Minutely.summary);
+
+                    //Hourly block: 
+                    string hourly_time = string.Empty;
+                    string hourly_summary = string.Empty;
+                    string hourly_precipIntensity = string.Empty;
+                    string hourly_precipProbability = string.Empty;
+                    string hourly_temperature = string.Empty;
+                    string hourly_apparentTemperature = string.Empty;
+                    string hourly_dewPoint = string.Empty;
+                    string hourly_humidity = string.Empty;
+                    string hourly_windSpeed = string.Empty;
+                    string hourly_windGust = string.Empty;
+                    string hourly_windBearing = string.Empty;
+                    string hourly_visibility = string.Empty;
+                    string hourly_cloudCover = string.Empty;
+                    string hourly_pressure = string.Empty;
+                    string hourly_ozone = string.Empty;
+                    string hourly_uvIndex = string.Empty;
+                    string hourly_precipType = string.Empty;
+
+                    foreach(var hour in json_response.Hourly.data)
+                    {
+                        hourly_time += json_response.Hourly.data.time.ToString() + ", ";
+                        hourly_summary += json_response.Hourly.data.summary + ", ";
+                        hourly_precipIntensity += json_response.Hourly.data.precipIntensity.ToString() + ", ";
+                        hourly_precipProbability += json_response.Hourly.data.precipProbability.ToString() + ", ";
+                        hourly_temperature += json_response.Hourly.data.temperature.ToString() + ", ";
+                        hourly_apparentTemperature += json_response.Hourly.data.apparentTemperature.ToString() + ", ";
+                        hourly_dewPoint += json_response.Hourly.data.dewPoint.ToString() + ", ";
+                        hourly_humidity += json_response.Hourly.data.humidity.ToString() + ", ";
+                        hourly_windSpeed += json_response.Hourly.data.windSpeed.ToString() + ", ";
+                        hourly_windGust += json_response.Hourly.data.windGust.ToString() + ", ";
+                        hourly_windBearing += json_response.Hourly.data.windBearing.ToString() + ", ";
+                        hourly_visibility += json_response.Hourly.data.visibility.ToString() + ", ";
+                        hourly_cloudCover += json_response.Hourly.data.cloudCover.ToString() + ", ";
+                        hourly_pressure += json_response.Hourly.data.pressure.ToString() + ", ";
+                        hourly_ozone += json_response.Hourly.data.ozone.ToString() + ", ";
+                        hourly_uvIndex += json_response.Hourly.data.uvIndex.ToString() + ", ";
+                        hourly_precipType += json_response.Hourly.data.precipType + ", ";
+                    }
+                    Insert.Parameters.AddWithValue("@hourly_time", hourly_time);
+                    Insert.Parameters.AddWithValue("@hourly_summary", hourly_summary);
+                    Insert.Parameters.AddWithValue("@hourly_precipIntensity", hourly_precipIntensity);
+                    Insert.Parameters.AddWithValue("@hourly_precipProbability", hourly_precipProbability);
+                    Insert.Parameters.AddWithValue("@hourly_temperature", hourly_temperature);
+                    Insert.Parameters.AddWithValue("@hourly_apparentTemperature", hourly_apparentTemperature;
+                    Insert.Parameters.AddWithValue("@hourly_dewPoint", hourly_dewPoint);
+                    Insert.Parameters.AddWithValue("@hourly_humidity", hourly_humidity);
+                    Insert.Parameters.AddWithValue("@hourly_windSpeed", hourly_windSpeed);
+                    Insert.Parameters.AddWithValue("@hourly_windGust", hourly_windGust);
+                    Insert.Parameters.AddWithValue("@hourly_windBearing", hourly_windBearing);
+                    Insert.Parameters.AddWithValue("@hourly_visibility", hourly_visibility);
+                    Insert.Parameters.AddWithValue("@hourly_cloudCover", hourly_cloudCover);
+                    Insert.Parameters.AddWithValue("@hourly_pressure", hourly_pressure);
+                    Insert.Parameters.AddWithValue("@hourly_ozone", hourly_ozone);
+                    Insert.Parameters.AddWithValue("@hourly_uvIndex", hourly_uvIndex);
+                    Insert.Parameters.AddWithValue("@hourly_precipType", hourly_precipType);
+                    Insert.Parameters.AddWithValue("@hourly_summary_no_list", json_response.Hourly.summary);
+
+                    
                 }
             }  
         }
